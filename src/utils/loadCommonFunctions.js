b3ccc43fb8213ebeeb7bf64751f4492622458257
Krampus B. <tiobarryx@gmail.com>
2024-12-08 16:23:29 +0100

    Update loadCommonFunctions.js

--- src/utils/loadCommonFunctions.js
+++ src/utils/loadCommonFunctions.js
@@ -3,10 +3,10 @@ const { extractDataFromMessage, baileysIs, download } = require(".");
 const { waitMessage } = require("./messages");
 const fs = require("fs");
 
-exports.loadCommonFunctions = ({ socket, messageData: initialMessageData }) => {
-  const { message } = initialMessageData;  // Utilizamos initialMessageData para evitar conflicto con 'messageData' dentro de la función
+exports.loadCommonFunctions = ({ socket, webMessage }) => {
   const {
     args,
+    message,
     commandName,
     fullArgs,
     fullMessage,
@@ -21,20 +21,20 @@ exports.loadCommonFunctions = ({ socket, messageData: initialMessageData }) => {
     return null;
   }
 
-  const isImage = baileysIs(message, "image");
-  const isVideo = baileysIs(message, "video");
-  const isSticker = baileysIs(message, "sticker");
+ const isImage = baileysIs(webMessage, "image");
+  const isVideo = baileysIs(webMessage, "video");
+  const isSticker = baileysIs(webMessage, "sticker");
 
-  const downloadImage = async (message, fileName) => {
-    return await download(message, fileName, "image", "png");
+  const downloadImage = async (webMessage, fileName) => {
+    return await download(webMessage, fileName, "image", "png");
   };
 
-  const downloadSticker = async (message, fileName) => {
-    return await download(message, fileName, "sticker", "webp");
+ const downloadSticker = async (webMessage, fileName) => {
+    return await download(webMessage, fileName, "sticker", "webp");
   };
 
-  const downloadVideo = async (message, fileName) => {
-    return await download(message, fileName, "video", "mp4");
+   const downloadVideo = async (webMessage, fileName) => {
+    return await download(webMessage, fileName, "video", "mp4");
   };
 
   const sendText = async (text, mentions) => {
@@ -49,16 +49,18 @@ exports.loadCommonFunctions = ({ socket, messageData: initialMessageData }) => {
   };
 
   const sendReply = async (text) => {
-    return await socket.sendMessage(remoteJid, {
-      text: `${BOT_EMOJI} ${text}`,
-    }, { quoted: message });
+    return await socket.sendMessage(
+      remoteJid,
+      { text: `${BOT_EMOJI} ${text}` },
+      { quoted: webMessage }
+    );
   };
 
   const sendReact = async (emoji) => {
     return await socket.sendMessage(remoteJid, {
       react: {
         text: emoji,
-        key: message.key,
+        key: webMessage.key,
       },
     });
   };
@@ -102,40 +104,40 @@ exports.loadCommonFunctions = ({ socket, messageData: initialMessageData }) => {
   const sendStickerFromFile = async (file) => {
     return await socket.sendMessage(remoteJid, {
       sticker: fs.readFileSync(file),
-    }, { quoted: message });
+    }, { quoted: webMessage });
   };
 
   const sendStickerFromURL = async (url) => {
     return await socket.sendMessage(remoteJid, {
       sticker: { url },
-    }, { url, quoted: message });
+    }, { url, quoted: webMessage });
   };
 
   const sendImageFromFile = async (file, caption = "") => {
     return await socket.sendMessage(remoteJid, {
       image: fs.readFileSync(file),
       caption: caption ? `${BOT_EMOJI} ${caption}` : "",
-    }, { quoted: message });
+    }, { quoted: webMessage });
   };
 
   const sendImageFromURL = async (url, caption = "") => {
     return await socket.sendMessage(remoteJid, {
       image: { url },
       caption: caption ? `${BOT_EMOJI} ${caption}` : "",
-    }, { url, quoted: message });
+    }, { url, quoted: webMessage });
   };
 
   const sendAudioFromURL = async (url) => {
     return await socket.sendMessage(remoteJid, {
       audio: { url },
       mimetype: "audio/mp4",
-    }, { url, quoted: message });
+    }, { url, quoted: webMessage });
   };
 
   const sendVideoFromURL = async (url) => {
     return await socket.sendMessage(remoteJid, {
       video: { url },
-    }, { url, quoted: message });
+    }, { url, quoted: webMessage });
   };
 
   const sendReplyOpenGroup = async (text) => {
