816249be093201609edc83b17583b6b66731d398
Krampus B. <tiobarryx@gmail.com>
2024-12-09 15:59:51 +0100

    Actualizar onMesssagesUpsert.js

--- src/middlewares/onMesssagesUpsert.js
+++ src/middlewares/onMesssagesUpsert.js
@@ -1,5 +1,6 @@
 const { dynamicCommand } = require("../utils/dynamicCommand");
 const { loadCommonFunctions } = require("../utils/loadCommonFunctions");
+const { isMuted } = require("../middlewares/mute");
 
 exports.onMessagesUpsert = async ({ socket, messages }) => {
   if (!messages.length) {
@@ -7,12 +8,15 @@ exports.onMessagesUpsert = async ({ socket, messages }) => {
   }
 
   for (const webMessage of messages) {
-    const commonFunctions = loadCommonFunctions({ socket, webMessage });
-
-    if (!commonFunctions) {
+    const userJid = webMessage.key.remoteJid;
+    if (await isMuted(userJid)) {
       continue;
     }
 
+    const commonFunctions = loadCommonFunctions({ socket, webMessage });
+    if (!commonFunctions) {
+      continue;
+    }
     await dynamicCommand(commonFunctions);
   }
 };
