6f13d4a02657fef538ef5c484616f64d7a73ba4b
Krampus B. <tiobarryx@gmail.com>
2024-12-09 18:02:28 +0100

    Actualizar silencio.js

--- src/commands/admin/silencio.js
+++ src/commands/admin/silencio.js
@@ -1,6 +1,6 @@
 const { PREFIX } = require("../../config");
 const { addMute, isUserMuted, removeMute } = require("../../utils/database");
-const { silence_times } = require("../../utils/database");
+const { SILENCE_TIMES } = require("../../utils/database");
 
 module.exports = {
   name: "silencio",
@@ -10,25 +10,21 @@ module.exports = {
   handle: async ({ client, message, args }) => {
     const { groupId, senderId, isAdmin, mentionedUsers } = message;
 
-    // Verificar si el usuario que ejecuta el comando es administrador
     if (!isAdmin) {
       return message.reply("🚫 Solo los administradores pueden ejecutar este comando.");
     }
 
-    // Verificar si hay un usuario mencionado
     if (mentionedUsers.length === 0) {
       return message.reply("⚠️ Por favor, menciona a un usuario para silenciar o desilenciar.");
     }
 
-    const userToMute = mentionedUsers[0]; // Primer usuario mencionado
-    const muteIndex = args[0]; // Primer argumento es el índice de tiempo
+    const userToMute = mentionedUsers[0];
+    const muteIndex = args[0];
 
     if (muteIndex === "0") {
-      // Desmutear al usuario
       if (!await isUserMuted(groupId, userToMute)) {
         return message.reply("⚠️ El usuario no está silenciado.");
       }
-
       removeMute(groupId, userToMute);
       return message.reply(`🔊 El usuario @${userToMute} ha sido desmuteado.`);
     }
@@ -37,13 +33,9 @@ module.exports = {
       return message.reply("⚠️ Parámetro inválido. Usa un número entre 1 y 5 para definir el tiempo de muteo.");
     }
 
-    // Obtener el tiempo de muteo desde el array silence_times
-    const muteDuration = silence_times[muteIndex.toString()];
+    const muteDuration = SILENCE_TIMES[muteIndex];
 
-    // Agregar el muteo
     addMute(groupId, userToMute, muteDuration);
-
-    // Responder con la confirmación
     message.reply(`🔇 El usuario @${userToMute} ha sido silenciado por ${muteDuration / 60000} minutos.`);
   },
 };
