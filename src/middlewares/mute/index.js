0185e3d21886f1759adf654b4487ccd995dcfe80
Krampus B. <tiobarryx@gmail.com>
2024-12-09 15:57:48 +0100

    Actualizar index.js

--- src/middlewares/mute/index.js
+++ src/middlewares/mute/index.js
@@ -6,7 +6,20 @@ exports.isMuted = async (userJid) => {
   return mutedUsers.includes(userJid);
 };
 
+exports.muteUser = async (userJid) => {
+  const db = await getDatabase();
+  const mutedUsers = await db.get('mutedUsers') || [];
+  if (!mutedUsers.includes(userJid)) {
+    mutedUsers.push(userJid);
+    await db.set('mutedUsers', mutedUsers);
+  }
+};
 
-
-
-
+exports.unmuteUser = async (userJid) => {
+  const db = await getDatabase();
+  const mutedUsers = await db.get('mutedUsers') || [];
+  if (mutedUsers.includes(userJid)) {
+    mutedUsers.splice(mutedUsers.indexOf(userJid), 1);
+    await db.set('mutedUsers', mutedUsers);
+  }
+};
