7511e5eda49678dc75e255e7246f22e5874f5117
Krampus B. <tiobarryx@gmail.com>
2024-12-14 23:53:31 +0100

    Actualizar ascender.js

--- src/commands/admin/ascender.js
+++ src/commands/admin/ascender.js
@@ -4,43 +4,25 @@ const { toggleAdmin } = require("../../utils/database");
 
 module.exports = {
   name: "admin",
-  description: "Asigna o quita permisos de administrador a un usuario",
-  commands: [
-    "admin",
-    "administrador",
-  ],
-  usage: `${PREFIX}admin (activar/desactivar) (userId)`,
-  handle: async ({ args, sendReply, sendSuccessReact, remoteJid }) => {
-    if (args.length !== 2) {
+  description: "Asigna o quita permisos de administrador",
+  commands: ["admin"],
+  usage: `${PREFIX}admin (promover/desconvertir)`,
+  handle: async ({ args, sendReply, sendSuccessReact, remoteJid, fromMe }) => {
+    if (!args.length || (args[0] !== "promover" && args[0] !== "desconvertir")) {
       throw new InvalidParameterError(
-        "👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Por favor, usa el formato correcto: `!admin (activar/desactivar) (userId)`"
+        "👻 Krampus.bot 👻 Escribe 'promover' o 'desconvertir' para gestionar los permisos de administrador."
       );
     }
 
-    const [action, userId] = args;
+    const action = args[0]; // Puede ser "promover" o "desconvertir"
 
-    // Validar que action sea "activar" o "desactivar"
-    if (action !== "activar" && action !== "desactivar") {
-      throw new InvalidParameterError(
-        "👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 El primer parámetro debe ser 'activar' o 'desactivar'."
-      );
+    if (action === "promover") {
+      await toggleAdmin(remoteJid, fromMe, "promover");
+    } else if (action === "desconvertir") {
+      await toggleAdmin(remoteJid, fromMe, "desconvertir");
     }
 
-    // Validar que userId sea un valor válido
-    if (!userId || userId.length === 0) {
-      throw new InvalidParameterError(
-        "👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Debes especificar el ID del usuario."
-      );
-    }
-
-    // Acción para promover o desconvertir
-    const actionType = action === "activar" ? "promover" : "desconvertir";
-    toggleAdmin(remoteJid, userId, actionType);
-
-    const context = action === "activar" ? "Asignados" : "Quitados";
     await sendSuccessReact();
-    await sendReply(
-      `👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Los permisos de administrador han sido ${context} al usuario ${userId} en el grupo.`
-    );
+    await sendReply(`👻 Krampus.bot 👻 El usuario ha sido ${action === "promover" ? "promovido" : "desconvertido"} a administrador.`);
   },
 };
