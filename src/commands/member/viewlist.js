6f39064eb1fc2187c7610f00a6256cb8d8601123
Krampus B. <tiobarryx@gmail.com>
2024-12-23 22:12:14 +0100

    Actualizar viewlist.js

--- src/commands/member/viewlist.js
+++ src/commands/member/viewlist.js
@@ -1,20 +1,19 @@
 const { PREFIX } = require("../../config");
-const { onlyNumbers } = require("../../utils");
-let userList = require("../../database/userList");
+const database = require("../../utils/database");
 
 module.exports = {
   name: "viewlist",
   description: "Muestra la lista de usuarios.",
   usage: `${PREFIX}viewlist`,
   handle: async ({ sendReply }) => {
-    if (userList.length === 0) {
+    if (database.userList.length === 0) {
       return await sendReply("👻 La lista está vacía.");
     }
 
-    const formattedList = userList
-      .map((jid, index) => `${index + 1}. @${onlyNumbers(jid)}`)
+    const formattedList = database.userList
+      .map((jid, index) => `${index + 1}. @${jid.split("@")[0]}`)
       .join("\n");
 
-    await sendReply(`👻 Lista de usuarios:\n\n${formattedList}`, { mentions: userList });
+    await sendReply(`👻 Lista de usuarios:\n\n${formattedList}`);
   },
 };
